dbfa59f6bc8a40cfb3edeb05ae1ebb037d24c0d8
fondjp <yokukame35@yahoo.co.jp>
2017-09-20 15:21:41 +0900

    next pips

--- tintirorin.js
+++ tintirorin.js
@@ -30,68 +30,6 @@ window.addEventListener("load", function () {
 
   }
 
-  function displayResult(myResult, enemyResult) {
-
-    if (myResult === 1) {
-      myHand.innerHTML += "あなたの目は3回とも揃いませんでした。目なしです。";
-    }
-
-    if (myResult === -1230) {
-      myHand.innerHTML += "あなたの目はヒフミです。";
-    }
-
-    if (myResult === 9999) {
-      myHand.innerHTML += "あなたの目はシゴロです！";
-    }
-
-    if (myResult % 100 === 0 && myResult < 10000) {
-      myHand.innerHTML += "あなたの目は" + (myResult / 100) + "です。";
-    }
-
-    if (myResult === 10000) {
-      myHand.innerHTML += "あなたの目はピンゾロです！！";
-    } else if (myResult % 10000 === 0 && myResult >= 20000) {
-      myHand.innerHTML += "あなたの目は" + (myResult / 10000) + "ゾロです！！";
-      myResult = 10000;
-    }
-
-    // ここまで自分の目の判定
-
-    if (enemyResult === 1) {
-      enemyHand.innerHTML += "敵の目は　　3回とも揃いませんでした。目なしです。"
-    }
-
-    if (enemyResult === -1230) {
-      enemyHand.innerHTML += "敵の目は　　ヒフミです！";
-    }
-
-    if (enemyResult === 9999) {
-      enemyHand.innerHTML += "敵の目は　　シゴロです・・・！";
-    }
-
-    if (enemyResult % 100 === 0 && enemyResult < 10000) {
-      enemyHand.innerHTML += "敵の目は　　" + (enemyResult / 100) + "です。";
-    }
-
-    if (enemyResult === 10000) {
-      enemyHand.innerHTML += "敵の目は　　ピンゾロです！！"
-    } else if (enemyResult % 10000 === 0 && enemyResult >= 20000) {
-      enemyHand.innerHTML += "敵の目は　　" + (enemyResult / 10000) + "ゾロです！！";
-      enemyResult = 10000; // 今回はピンゾロも倍率は同じであるため、直す
-    }
-
-    // ここまで相手の目の判定
-
-    if (myResult > enemyResult) {
-      isWon.innerHTML = "あなたの勝ちです！";
-    } else if (myResult === enemyResult) {
-      isWon.innerHTML = "引き分けです。";
-    } else {
-      isWon.innerHTML = "あなたの負けです…";
-    }
-
-  }
-
   function battle(myResult, enemyResult, coin, betCoin) {
     var bairitsu = 1;
 
@@ -117,7 +55,6 @@ window.addEventListener("load", function () {
       bairitsu *= -1;
     }
 
-    output.innerHTML += "デバッグ用：　倍率は " + bairitsu + " 倍";
     if (bairitsu === -1) {
       return coin -= betCoin;
     } else {
@@ -129,12 +66,6 @@ window.addEventListener("load", function () {
     var tintirorinCounter = 0;
     while (tintirorinCounter < 2) {
 
-      if (tintirorinCounter === 0) {
-        output.innerHTML += "あなたの出目は・・・";
-      } else {
-        output.innerHTML += "敵の出目は・・・";
-      }
-
       var pips = [];
       var storm = 0; // アラシを直訳ｗｗ　3つの目が同じだった時、その数字を値にして返す
       var hand = 0;
@@ -144,9 +75,22 @@ window.addEventListener("load", function () {
       var times = 1; // 回数表示
 
       while (menashi < 3) {
+
+        howManyTimes.innerHTML += times + "回目";
+
         for (var i = 0; i < 3; i++) { // 3つのサイコロを振って
           pips.push(Math.floor(Math.random() * 6) + 1); // 配列の中に結果を入れる
         };
+
+        if (tintirorinCounter === 0) {
+          myPipsResultFirst.innerHTML = pips[0];
+          myPipsResultSecond.innerHTML = pips[1];
+          myPipsResultThird.innerHTML = pips[2];
+        } else {
+          enemyPipsResultFirst.innerHTML = pips[0];
+          enemyPipsResultSecond.innerHTML = pips[1];
+          enemyPipsResultThird.innerHTML = pips[2];
+        }
 
         pips.sort(function (a, b) { // その三つの数字を小さい順に並べる
           if (a < b) return -1;
@@ -156,36 +100,73 @@ window.addEventListener("load", function () {
 
         if (pips[0] !== pips[1] !== pips[2]) {
           menashi++;
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = "目なし";
+          } else {
+            enemyHand.innerHTML = "目なし";
+          }
+
         }
 
         if (pips[0] === pips[2]) { // 0番目と2番目が同じ。それすなわち1番目も同じ！！
           menashi = 4;
           storm = pips[0];
-          pipsResult.innerHTML += times + "回目 " + pips.join(", ");
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = pips[0] + "ゾロ";
+          } else {
+            enemyHand.innerHTML = pips[0] + "ゾロ";
+          }
+
           break; // handの判定が引っかかるのでbreakで避難。一応専用のconsole.log()を追加。
         }
 
         if (pips[0] === pips[1]) {
           menashi = 4;
           hand = pips[2];
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = pips[2] + "の出目";
+          } else {
+            enemyHand.innerHTML = pips[2] + "の出目";
+          }
+
         } else if (pips[1] === pips[2]) {
           menashi = 4;
           hand = pips[0];
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = pips[0] + "の出目";
+          } else {
+            enemyHand.innerHTML = pips[0] + "の出目";
+          }
+
         }
 
         if (pips[0] === 4 && pips[1] === 5 && pips[2] === 6) {
           menashi = 4;
           shigoro = 1;
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = "シゴロ";
+          } else {
+            enemyHand.innerHTML = "シゴロ";
+          }
+
         }
 
         if (pips[0] === 1 && pips[1] === 2 && pips[2] === 3) {
           menashi = 4;
           hihumi = 1;
+
+          if (tintirorinCounter === 0) {
+            myHand.innerHTML = "ヒフミ";
+          } else {
+            enemyHand.innerHTML = "ヒフミ";
+          }
+
         }
-
-
-        pipsResult.innerHTML += times + "回目 " + pips.join(", ");
-
 
         pips = [];
         times++;
@@ -204,15 +185,22 @@ window.addEventListener("load", function () {
 
       tintirorinCounter++;
       times = 1;
-    }
 
-    displayResult(myResult, enemyResult);
+    }
 
     if (myResult % 10000 === 0 && myResult >= 20000) {
       myResult = 10000; // 今回はピンゾロも倍率は同じであるため、直す
     }
     if (enemyResult % 10000 === 0 && enemyResult >= 20000) {
       enemyResult = 10000;
+    }
+
+    if (myResult > enemyResult) {
+      isWon.innerHTML = "あなたの勝ちです！";
+    } else if (myResult === enemyResult) {
+      isWon.innerHTML = "引き分けです。";
+    } else {
+      isWon.innerHTML = "あなたの負けです…";
     }
 
     console.log(myResult, enemyResult, coin, betCoin);
@@ -226,30 +214,52 @@ window.addEventListener("load", function () {
   };
 
   start.addEventListener("click", function () {
+
     var betCoin = document.getElementById("betCoin").value;
+    var howManyTimes = document.getElementById("howManyTimes");
     var again = document.getElementById("again");
     var myHand = document.getElementById("myHand");
     var enemyHand = document.getElementById("enemyHand");
     var isWon = document.getElementById("isWon");
+    var errorLog = document.getElementById("errorLog");
+    var myPipsResultFirst = document.getElementById("myPipsResultFirst");
+    var myPipsResultSecond = document.getElementById("myPipsResultSecond");
+    var myPipsResultThird = document.getElementById("myPipsResultThird");
+    var enemyPipsResultFirst = document.getElementById("enemyPipsResultFirst");
+    var enemyPipsResultSecond = document.getElementById("enemyPipsResultSecond");
+    var enemyPipsResultThird = document.getElementById("enemyPipsResultThird");
+
     betCoin = Number(betCoin);
-    console.log(betCoin)
-    if (coin <= 0) {
-      output.innerHTML = "<p>ゲームオーバー！</p>";
+    if (coin <= 0 || coin >= 50000000000000000) {
+
+      if (coin <= 0) {
+        document.getElementById("gameOver").style.display = "block";
+      } else {
+        document.getElementById("clear").style.display = "block";
+      }
+
     } else if (coin - betCoin < 0) {
-      output.innerHTML = "<p>金額が不足しています。</p>";
+      errorLog.innerHTML = "金額が不足しています。";
     } else if (isNaN(betCoin) || betCoin <= 0) {
-      output.innerHTML = "<p>正しい数値を入力してください。</p>";
+      errorLog.innerHTML = "正しい数値を入力してください。";
     } else {
+      howManyTimes.innerHTML = "";
       output.innerHTML = "";
-      pipsResult.innerHTML = "";
+      myPipsResultFirst.innerHTML = "";
+      myPipsResultSecond.innerHTML = "";
+      myPipsResultThird.innerHTML = "";
+      enemyPipsResultFirst.innerHTML = "";
+      enemyPipsResultSecond.innerHTML = "";
+      enemyPipsResultThird.innerHTML = "";
       myHand.innerHTML = "";
       enemyHand.innerHTML = "";
       isWon.innerHTML = "";
+      errorLog = "";
       coin = tintirorin(coin, betCoin);
-      myCoin.innerHTML = "<p>現在の所持金：" + coin + "</p>";
+      myCoin.innerHTML = "現在の所持金：" + coin;
     }
   })
 
-  myCoin.innerHTML = "<p>現在の所持金：" + coin + "</p>";
+  myCoin.innerHTML = "現在の所持金：" + coin;
 
 });
